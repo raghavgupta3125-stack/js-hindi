f8d9c89603174bf0636dbec1f643e2f851d53efc
raghavgupta3125-stack <raghavgupta3125@gmail.com>
2026-06-03 13:06:42 +0000

    content

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "John";
+let age = 25; 
+let state;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+const accontId = 1445
+let accEmail = "xyz@emai"
+var accPass = "1245"
+let accCity = "jaipur"
+let accState
+accPass = "2121"
+accEmail = "abc2gmail"
+accCity = "delhi"
+
+console.log(accontId);
+console.table([accPass, accEmail, accCity, accState])
